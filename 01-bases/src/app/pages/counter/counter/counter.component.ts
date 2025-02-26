21419372c9cd8ab7f8c087adda96b36e4ee5b12c
caindra <mmirgal868@g.educaand.es>
2025-02-26 16:12:55 +0100

    more of signals

--- 01-bases/src/app/pages/counter/counter/counter.component.ts
+++ 01-bases/src/app/pages/counter/counter/counter.component.ts
@@ -1,14 +1,19 @@
-import { Component, signal, WritableSignal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   imports: [],
   templateUrl: './counter.component.html',
-  styles: ``
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CounterComponent {
   public counter: number = 10;
   public counterSignal: WritableSignal<number> = signal(10);
+
+  constructor(){
+    //setInterval(() => {console.log('tick')}, 2000);
+  }
 
   increaseBy(value: number){
     this.counter += value;
