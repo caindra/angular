bb3187724071b6ed2d6172690775cfd17696ace6
caindra <mmirgal868@g.educaand.es>
2025-03-04 19:15:31 +0100

    inputs - add characters

--- 01-bases/src/app/pages/acotar/acotar-page/acotar-page.component.ts
+++ 01-bases/src/app/pages/acotar/acotar-page/acotar-page.component.ts
@@ -9,70 +9,48 @@ interface Character {
 
 @Component({
   selector: 'app-acotar-page',
-  imports: [
-    NgClass
-  ],
+  standalone: true,
+  imports: [NgClass],
   templateUrl: './acotar-page.component.html',
   styles: ``
 })
 export class AcotarPageComponent {
+
+  name = signal('Nesta');
+  power = signal(90);
+
   public characters = signal<Character[]>([
-    {
-      id: 1,
-      name: "Feyre Archeron",
-      power: 95,
-    },
-    {
-      id: 2,
-      name: "Rhysand",
-      power: 98,
-    },
-    {
-      id: 3,
-      name: "Morrigan",
-      power: 92,
-    },
-    {
-      id: 4,
-      name: "Cassian",
-      power: 94,
-    },
-    {
-      id: 5,
-      name: "Nesta Archeron",
-      power: 90,
-    },
-    {
-      id: 6,
-      name: "Azriel",
-      power: 93,
-    },
-    {
-      id: 7,
-      name: "Amren",
-      power: 97,
-    },
-    {
-      id: 8,
-      name: "Tamlin",
-      power: 85,
-    },
-    {
-      id: 9,
-      name: "Lucien",
-      power: 88,
-    },
-    {
-      id: 10,
-      name: "Elain Archeron",
-      power: 40,
-    }
+    { id: 1, name: "Feyre Archeron", power: 95 },
+    { id: 2, name: "Rhysand", power: 98 },
+    { id: 3, name: "Morrigan", power: 92 },
+    { id: 4, name: "Cassian", power: 94 },
+    { id: 5, name: "Nesta Archeron", power: 90 },
+    { id: 6, name: "Azriel", power: 93 },
+    { id: 7, name: "Amren", power: 97 },
+    { id: 8, name: "Tamlin", power: 85 },
+    { id: 9, name: "Lucien", power: 88 },
+    { id: 10, name: "Elain Archeron", power: 40 }
   ]);
 
-  //to be able to use NgClass:
-  powerClasses = computed(() => {
-    return {
-      'text-success': true,
-    }
-  });
+  powerClasses = computed(() => ({
+    'text-success': this.power() > 80
+  }));
+
+  addCharacter(): void {
+    if (!this.name() || this.power() < 0) return;
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power()
+    };
+
+    this.characters.update(list => [...list, newCharacter]);
+    this.resetFields();
+  }
+
+  resetFields(): void {
+    this.name.set('');
+    this.power.set(0);
+  }
 }
