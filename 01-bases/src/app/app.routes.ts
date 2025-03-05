68fcaf1305ebdc1b7549b9b1426f1f13da80d689
caindra <mmirgal868@g.educaand.es>
2025-03-05 17:02:27 +0100

    replicate acotar page into acotar super

--- 01-bases/src/app/app.routes.ts
+++ 01-bases/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { CounterComponent } from './pages/counter/counter/counter.component';
 import { HeroPageComponent } from './pages/hero/hero-page/hero-page.component';
-import { AcotarPageComponent } from './pages/acotar/acotar-page/acotar-page.component';
+import { AcotarPageComponent } from './pages/acotar/acotar-page.component';
+import { AcotarSuperPageComponent } from './pages/acotar-super/acotar-super-page.component';
 
 export const routes: Routes = [
   {
@@ -15,6 +16,10 @@ export const routes: Routes = [
   {
     path: 'acotar',
     component: AcotarPageComponent
+  },
+  {
+    path: 'acotar-super',
+    component: AcotarSuperPageComponent
   },
   {
     path: '**',
---
+++ 01-bases/src/app/pages/acotar-super/acotar-super-page.component.ts
@@ -0,0 +1,42 @@
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
+
+@Component({
+  selector: 'acotar-super',
+  templateUrl: './acotar-super-page.component.html',
+  styles: ``
+})
+export class AcotarSuperPageComponent {
+
+  name = signal('');
+  power = signal(0);
+
+  public characters = signal<Character[]>([
+    { id: 1, name: "Feyre Archeron", power: 95 },
+    { id: 2, name: "Rhysand", power: 98 },
+  ]);
+
+  addCharacter(): void {
+    if (!this.name() || this.power() < 0) return;
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power()
+    };
+
+    this.characters.update(list => [...list, newCharacter]);
+    this.resetFields();
+  }
+
+  resetFields(): void {
+    this.name.set('');
+    this.power.set(0);
+  }
+}
