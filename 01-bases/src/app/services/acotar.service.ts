4914f34224dee9ac6a12f44b475fb8622c588ea7
caindra <maria31mx@gmail.com>
2025-03-07 09:15:47 +0100

    effects + LocalStorage

--- 01-bases/src/app/services/acotar.service.ts
+++ 01-bases/src/app/services/acotar.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
 @Injectable({
@@ -9,6 +9,10 @@ export class AcotarService {
     { id: 1, name: "Feyre Archeron", power: 95 },
     { id: 2, name: "Rhysand", power: 98 },
   ]);
+
+  saveToLocalStorage = effect(() => {
+    localStorage.setItem('characters', JSON.stringify(this.characters()));
+  })
 
   addCharacter(newCharacter: Character): void {
     this.characters.update(list => [...list, newCharacter]);
