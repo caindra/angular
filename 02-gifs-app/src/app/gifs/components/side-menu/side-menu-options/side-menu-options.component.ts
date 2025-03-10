da72ed49c2bd41ad5fa6991604bb6cbfb47e928e
caindra <mmirgal868@g.educaand.es>
2025-03-10 09:13:12 +0100

    routed children components on side menu

---
+++ 02-gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+  icon: string;
+  label: string;
+  route: string;
+  subLabel: string;
+}
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+  ],
+  templateUrl: './side-menu-options.component.html',
+  styles: ``
+})
+export class SideMenuOptionsComponent {
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs populares',
+      route: '/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Search',
+      subLabel: 'Buscar gifs',
+      route: '/dashboard/search',
+    },
+  ];
+}
