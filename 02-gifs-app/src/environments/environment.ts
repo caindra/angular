054d5d14d1b0c5a2bd6f67a35513e82c752fe3d1
caindra <mmirgal868@g.educaand.es>
2025-03-10 13:12:36 +0100

    GifsService - interfaces

--- 02-gifs-app/src/environments/environment.ts
+++ 02-gifs-app/src/environments/environment.ts
@@ -5,6 +5,8 @@ export const environment = {
   companySlogan: 'Visualiza tus gifs',
 
   //ApiKeys
+  giphyApiKey: 'rC3wTuEpqz4jiQfGUXsP0CTwFbbitKP4',
+  giphyUrl: 'https://api.giphy.com/v1',
 
   //URLs
 };
