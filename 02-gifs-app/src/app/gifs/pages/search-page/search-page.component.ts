dc48437b363bce0213b91e0cfdee0950d163bc39
caindra <mmirgal868@g.educaand.es>
2025-03-10 16:37:15 +0100

    show searched gifs

--- 02-gifs-app/src/app/gifs/pages/search-page/search-page.component.ts
+++ 02-gifs-app/src/app/gifs/pages/search-page/search-page.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ListComponent } from "../../components/list/list.component";
+import { GifService } from '../../services/gifs.service';
+import { Gif } from '../../interfaces/gif.interface';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [ListComponent],
   templateUrl: './search-page.component.html',
   styles: ``
 })
 export default class SearchPageComponent {
+  gifService =  inject(GifService);
+  gifs = signal<Gif[]>([]);
 
+  onSearch(query: string){
+    this.gifService.searchGifs(query).subscribe((response) => {
+      this.gifs.set(response);
+    });
+  }
 }
--- 02-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 02-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -4,6 +4,7 @@ import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { Gif } from '../interfaces/gif.interface';
 import { GifMapper } from '../mapper/gif.mapper';
+import { map } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class GifService {
@@ -28,8 +29,28 @@ export class GifService {
       const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
       this.trendingGifs.set(gifs);
       this.trendingGifsLoading.set(false);
-      console.log({gifs});
     });
+  }
+
+  searchGifs(query: string) {
+    return this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
+      params: {
+        api_key: environment.giphyApiKey,
+        q: query,
+        limit: 25,
+        offset: 0,
+        rating: 'g'
+      }
+    }).pipe(
+      map(({data}) => data),
+      map((items) => GifMapper.mapGiphyItemsToGifArray(items))
+
+      // TODO: Historial
+    );
+    // .subscribe( (resp) => {
+    //   const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+    //   return gifs;
+    // });
   }
 
 }
