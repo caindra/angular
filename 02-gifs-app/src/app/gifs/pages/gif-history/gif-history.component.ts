fb5cbaba5a0871a72898c56ccd366d2e7f5050f7
caindra <mmirgal868@g.educaand.es>
2025-03-13 12:19:01 +0100

    show history on search

--- 02-gifs-app/src/app/gifs/pages/gif-history/gif-history.component.ts
+++ 02-gifs-app/src/app/gifs/pages/gif-history/gif-history.component.ts
@@ -1,21 +1,23 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map, Observable } from 'rxjs';
+import { GifService } from '../../services/gifs.service';
+import { ListComponent } from "../../components/list/list.component";
 
 @Component({
   selector: 'app-gif-history',
-  imports: [],
+  imports: [ListComponent],
   templateUrl: './gif-history.component.html',
   styles: ``
 })
 export default class GifHistoryComponent {
-  // query = inject(ActivatedRoute).params.subscribe(params => {
-  //   console.log({params});
-  // });
+  gifService = inject(GifService);
 
   query = toSignal(inject(ActivatedRoute).params.pipe(
     map(params => params['query'])
   ));
+
+  gifsByKey = computed(() => this.gifService.getHistoryGifs(this.query()));
 }
 
