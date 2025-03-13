b3f5df00ac11c0778ccc797254567c50d6410475
caindra <mmirgal868@g.educaand.es>
2025-03-13 16:51:41 +0100

    take references from template

--- 02-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
+++ 02-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,21 +1,6 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, computed, ElementRef, inject, signal, viewChild } from '@angular/core';
 import { ListComponent } from '../../components/list/list.component';
 import { GifService } from '../../services/gifs.service';
-
-// const imageUrls: string[] = [
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
-// ];
 
 @Component({
   selector: 'app-trending-page',
@@ -28,4 +13,9 @@ import { GifService } from '../../services/gifs.service';
 export default class TrendingPageComponent {
   gifService = inject(GifService);
 
+  scrollDivRef = viewChild<ElementRef>('groupDiv')
+
+  onScroll(event: Event) {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+  }
 }
