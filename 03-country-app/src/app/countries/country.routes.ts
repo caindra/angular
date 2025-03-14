536efbe115b75a52181d6074fa21857f792b875d
caindra <maria31mx@gmail.com>
2025-03-14 13:34:30 +0100

    changed from scss to css. created new initial pages + provisional routes

---
+++ 03-country-app/src/app/countries/country.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+
+export const countryRoutes: Routes = [
+  {
+    path: '',
+    component: ByCapitalPageComponent
+  },
+
+  {
+    path: '**',
+    redirectTo: ''
+  },
+];
+
+export default countryRoutes;
