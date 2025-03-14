08995f9eca2d9793a40da214a145c38aab958114
caindra <maria31mx@gmail.com>
2025-03-14 08:50:49 +0100

    determined the final of the scroll action

--- 02-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
+++ 02-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -13,9 +13,17 @@ import { GifService } from '../../services/gifs.service';
 export default class TrendingPageComponent {
   gifService = inject(GifService);
 
-  scrollDivRef = viewChild<ElementRef>('groupDiv')
+  scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv')
 
   onScroll(event: Event) {
     const scrollDiv = this.scrollDivRef()?.nativeElement;
+
+    if(!scrollDiv) return;
+
+    const scrollTop = scrollDiv.scrollTop;
+    const clientHeight = scrollDiv.clientHeight;
+    const scrollHeight = scrollDiv.scrollHeight;
+
+    const isAtBottom = scrollTop + clientHeight >= scrollHeight;
   }
 }
