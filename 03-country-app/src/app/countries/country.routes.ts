df597168242399a8c2ae9945bca9f750fadfd5b7
caindra <maria31mx@gmail.com>
2025-03-14 14:38:07 +0100

    layout components

--- 03-country-app/src/app/countries/country.routes.ts
+++ 03-country-app/src/app/countries/country.routes.ts
@@ -1,16 +1,27 @@
 import { Routes } from '@angular/router';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { CountryLayoutComponent } from './layout/country-layout/country-layout.component';
 
 export const countryRoutes: Routes = [
   {
     path: '',
-    component: ByCapitalPageComponent
+    component: CountryLayoutComponent,
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapitalPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'by-capital'
+      },
+    ]
   },
 
-  {
-    path: '**',
-    redirectTo: ''
-  },
+  // {
+  //   path: '**',
+  //   redirectTo: ''
+  // },
 ];
 
 export default countryRoutes;
