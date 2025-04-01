93fb6e073aaa2a7b2e23590f75a970c84fa4fa9c
caindra <maria31mx@gmail.com>
2025-04-01 08:37:01 +0200

    debounce effect for automatic searchs

--- 03-country-app/src/app/countries/components/search-input/search-input.component.ts
+++ 03-country-app/src/app/countries/components/search-input/search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -7,8 +7,19 @@ import { Component, input, output } from '@angular/core';
   styles: ``
 })
 export class SearchInputComponent {
-
   placeholder = input('Buscar');
   value = output<string>();
+  debounceTime = input(300);
 
+  inputValue = signal<string>('');
+
+  debounceEffect = effect((onCleanup) => {
+    const value = this.inputValue();
+
+    const timeOut = setTimeout(() => {this.value.emit(value)}, this.debounceTime());
+
+    onCleanup(() => {
+      clearTimeout(timeOut);
+    });
+  });
 }
