c3606b46876738da96b3efa5ec98daa50f5c9bdb
caindra <mmirgal868@g.educaand.es>
2025-04-07 16:03:41 +0200

    i18nselect pipe

--- 04-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
+++ 04-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,10 +1,41 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { CardComponent } from "../../components/card/card.component";
+import { I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'Atlas',
+  gender: 'male',
+  age: '32',
+  address: 'Toronto, Canada'
+};
+
+const client2 = {
+  name: 'Caindra',
+  gender: 'female',
+  age: '27',
+  address: 'Toronto, Canada',
+};
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent],
+  imports: [CardComponent, I18nSelectPipe],
   templateUrl: './uncommon-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class UncommonPageComponent { }
+export default class UncommonPageComponent {
+  //i18n Select
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
+
+  changeClient() {
+    if (this.client() === client1) {
+      this.client.set(client2);
+    } else {
+      this.client.set(client1);
+    }
+  }
+}
