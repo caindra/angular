5d4e76fc3109e57661b40ac2aa026a19edc8e430
caindra <mmirgal868@g.educaand.es>
2025-04-08 18:52:40 +0200

    canfly and herocolor pipes

--- 04-pipes-app/src/app/pages/custom-page/custom-page.component.ts
+++ 04-pipes-app/src/app/pages/custom-page/custom-page.component.ts
@@ -1,11 +1,16 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import { ToggleCasePipe } from '../../pipes/toggle-case-pipe';
+import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from '../../data/heores.data';
+import { CanFlyPipe } from '../../pipes/can-fly.pipe';
+import { HeroColorPipe } from '../../pipes/hero-color.pipe';
 
 @Component({
   selector: 'app-custom-page',
   imports: [
     ToggleCasePipe,
+    CanFlyPipe,
+    HeroColorPipe,
+    
   ],
   templateUrl: './custom-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
