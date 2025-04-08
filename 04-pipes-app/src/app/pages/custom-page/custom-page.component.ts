db56add38eb4671df7b9561ead94a04444de68e2
caindra <mmirgal868@g.educaand.es>
2025-04-08 19:06:15 +0200

    herotextcolor & hero creator pipes

--- 04-pipes-app/src/app/pages/custom-page/custom-page.component.ts
+++ 04-pipes-app/src/app/pages/custom-page/custom-page.component.ts
@@ -3,6 +3,9 @@ import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from '../../data/heores.data';
 import { CanFlyPipe } from '../../pipes/can-fly.pipe';
 import { HeroColorPipe } from '../../pipes/hero-color.pipe';
+import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
+import { TitleCasePipe } from '@angular/common';
+import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
 
 @Component({
   selector: 'app-custom-page',
@@ -10,7 +13,9 @@ import { HeroColorPipe } from '../../pipes/hero-color.pipe';
     ToggleCasePipe,
     CanFlyPipe,
     HeroColorPipe,
-    
+    HeroTextColorPipe,
+    TitleCasePipe,
+    HeroCreatorPipe,
   ],
   templateUrl: './custom-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
