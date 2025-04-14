d095451b00bde95041179719a9271b8f2093b1b1
caindra <mmirgal868@g.educaand.es>
2025-04-14 13:05:28 +0200

    dynamic forms with array

--- 05-reactive-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 05-reactive-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,14 +1,27 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
-  imports: [
-    JsonPipe,
-  ],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './dynamic-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class DynamicPageComponent {
+  private fb = inject(FormBuilder);
 
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Persona 5 Royal', Validators.required],
+      ['Tears Of The Kingdom', Validators.required],
+    ],
+      Validators.minLength(3)
+    ),
+  });
+
+  get favoriteGames(){
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
 }
