ca4f2e0a81ddc3b639e96f40da9b490cad8514eb
caindra <mmirgal868@g.educaand.es>
2025-04-14 11:29:33 +0200

    show errors on screen - improve UX

--- 05-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 05-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -10,7 +10,7 @@ import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } 
 export class BasicPageComponent {
   private formBuilder = inject(FormBuilder);
 
-  myForm = this.formBuilder.group({
+  myForm: FormGroup = this.formBuilder.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     price: [0, [Validators.required, Validators.minLength(10)]],
     inStorage: [0, [Validators.required, Validators.minLength(0)]],
@@ -23,4 +23,42 @@ export class BasicPageComponent {
   //   price: new FormControl(0),
   //   inStorage: new FormControl(0),
   // });
+
+  isValidField(fieldName: string): boolean | null{
+    return (
+      this.myForm.controls[fieldName].errors &&
+      this.myForm.controls[fieldName].touched
+    );
+  }
+
+  getFieldError(fieldName: string): string | null{
+    if (!this.myForm.controls[fieldName]) return null;
+
+    const errors = this.myForm.controls[fieldName].errors ?? {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido.';
+        case 'minlength':
+          return `Mínimo de ${errors['minlength'].requiredLength} carateres.`;
+        case 'required':
+          return `Valor mínimo de ${errors['min'].min} carateres.`;
+      }
+    }
+
+    return null;
+  }
+
+  onSave(){
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    this.myForm.reset({
+      price: 0,
+      inStorage: 0,
+    });
+  }
 }
