ed637982993eaabfec417d6f7fcf22313003c31b
caindra <mmirgal868@g.educaand.es>
2025-04-14 16:58:39 +0200

    add / delete form controls + prevent default behaviour

--- 05-reactive-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 05-reactive-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
@@ -13,18 +13,37 @@ export class DynamicPageComponent {
   private fb = inject(FormBuilder);
   formUtils = FormUtils;
 
+  newFavoriteGame = new FormControl('', Validators.required);
+  // newFavoriteGame = this.fb.control('', Validators.required);
+
   myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
-    favoriteGames: this.fb.array([
-      ['Persona 5 Royal', Validators.required],
-      ['Tears Of The Kingdom', Validators.required],
-    ],
+    favoriteGames: this.fb.array(
+      [
+        ['Persona 5 Royal', Validators.required],
+        ['Tears Of The Kingdom', Validators.required],
+      ],
       Validators.minLength(3)
     ),
   });
 
-  get favoriteGames(){
+  get favoriteGames() {
     return this.myForm.get('favoriteGames') as FormArray;
   }
 
+  onAddToFavorites() {
+    if(this.newFavoriteGame.invalid) return;
+
+    const newGame = this.newFavoriteGame.value;
+
+    this.favoriteGames.push(this.fb.control(newGame, Validators.required));
+  }
+
+  onDeleteFavorite(index: number){
+    this.favoriteGames.removeAt(index);
+  }
+
+  onSubmit(){
+    this.myForm.markAllAsTouched();
+  }
 }
