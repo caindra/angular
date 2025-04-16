3e2cbda1b4f687cc0e8e635ab4afa85d3d72d4e6
caindra <mmirgal868@g.educaand.es>
2025-04-16 11:43:10 +0200

    first selector complete + subscribed to form changes

--- 05-reactive-forms-app/src/app/country/pages/country-page/country-page.component.ts
+++ 05-reactive-forms-app/src/app/country/pages/country-page/country-page.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, effect, inject, signal } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
+import { CountryService } from '../../services/country.service';
+import { Country } from '../../interfaces/country.interface';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './country-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class CountryPageComponent {
+  private fb = inject(FormBuilder);
+  countryService = inject(CountryService);
+  formUtils = FormUtils;
 
+  regions = signal(this.countryService.regions);
+
+  countriesByRegion = signal<Country[]>([]);
+  borders = signal<Country[]>([]);
+
+  myForm: FormGroup = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    border: ['', Validators.required],
+  });
+
+  onFormChanged = effect((onCleanUp) => {
+    const formRegionChanged = this.onRegionChanged();
+
+    onCleanUp(() => {
+      formRegionChanged.unsubscribe();
+    });
+  });
+
+  onRegionChanged() {
+    return this.myForm
+      .get('region')!
+      .valueChanges.pipe(
+        tap(() => this.myForm.get('country')!.setValue('')),
+        tap(() => this.myForm.get('border')!.setValue('')),
+        tap(() => {
+          this.borders.set([]);
+          this.countriesByRegion.set([]);
+        }),
+        switchMap(region => this.countryService.getCountriesByRegion(region! ?? ''))
+      )
+      .subscribe((countries) => {
+        this.countriesByRegion.set(countries);
+      });
+  }
 }
