347493fddead5790b5695fa02209efc9839e68c4
caindra <maria31mx@gmail.com>
2025-04-23 09:10:31 +0200

    create environments via script + fixed style import errors

--- 07-maps-app/scripts/set-envs.js
+++ 07-maps-app/scripts/set-envs.js
@@ -2,13 +2,21 @@ const {writeFileSync, mkdirSync} = require('fs');
 
 require('dotenv').config();
 
-const targetPath = '.src/environments/environment.ts'
-const targetPathDev = '.src/environments/environment.development.ts'
+const targetPath = '.src/environments/environment.ts';
+const targetPathDev = '.src/environments/environment.development.ts';
 
-if(!process.env['MAPBOX_KEY']){
-  throw new Error('MAPBOX_KEY is not set');
-}
+// const mapboxKey = process.env['MAPBOX_KEY'];
 
-const envFileContent = `
- 
-`;
+// if(!mapboxKey){
+//   throw new Error('MAPBOX_KEY is not set');
+// }
+
+// const envFileContent = `
+//  export const environment = {
+//   mapboxKey: "${mapboxKey}"
+//  }
+// `;
+
+mkdirSync('./src/environments', {recursive: true});
+writeFileSync(targetPath, envFileContent);
+writeFileSync(targetPathDev, envFileContent);
