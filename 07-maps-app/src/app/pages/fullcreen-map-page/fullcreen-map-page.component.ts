22a42ec48fc98cee3f41016e1d0b6e4a909fef45
caindra <maria31mx@gmail.com>
2025-04-23 10:32:25 +0200

    map on fullscreen

--- 07-maps-app/src/app/pages/fullcreen-map-page/fullcreen-map-page.component.ts
+++ 07-maps-app/src/app/pages/fullcreen-map-page/fullcreen-map-page.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, viewChild } from '@angular/core';
+import { MapaComponent } from '../../components/mapa/mapa.component';
 
 @Component({
   selector: 'app-fullcreen-map-page',
-  imports: [],
+  imports: [
+    MapaComponent,
+  ],
   templateUrl: './fullcreen-map-page.component.html',
-  styles: ``
+  styles: `
+    div {
+      width: 100vw;
+      height: calc(100vh - 64px);
+    }
+  `
 })
-export class FullcreenMapPageComponent {
+export class FullcreenMapPageComponent implements AfterViewInit{
+  divElement = viewChild<ElementRef>('map');
 
+  async ngAfterViewInit() {
+    if(!this.divElement()?.nativeElement) return;
+
+    await new Promise(resolve => setTimeout(resolve, 80));
+
+    const element = this.divElement()!.nativeElement;
+    console.log(element);
+    
+  }
 }
