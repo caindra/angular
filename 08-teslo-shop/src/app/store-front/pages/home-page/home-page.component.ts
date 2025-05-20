c836abb3461dcf2ddf0d446e25d2ad22e9fa77da
caindra <mmirgal868@g.educaand.es>
2025-05-20 20:10:37 +0200

    env variables + parameters

--- 08-teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
+++ 08-teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
@@ -16,7 +16,7 @@ export class HomePageComponent {
   productsResource = rxResource({
     request: () => ({}),
     loader:({request}) => {
-      return this.productsService.getProducts();
+      return this.productsService.getProducts({});
     }
   })
 }
