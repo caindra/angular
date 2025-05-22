628d116145ca24df46ea66deeebaee0ab0536aff
caindra <mmirgal868@g.educaand.es>
2025-05-22 11:24:06 +0200

    image carousel + gender page by gender

--- 08-teslo-shop/src/app/store-front/pages/gender-page/gender-page.component.ts
+++ 08-teslo-shop/src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal, rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
+import { map } from 'rxjs';
+import { ProductCardComponent } from "../../../products/components/product-card/product-card.component";
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './gender-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class GenderPageComponent {
+  route = inject(ActivatedRoute);
+  productsService = inject(ProductsService);
 
+  gender = toSignal(this.route.params.pipe(map(({ gender }) => gender)));
+
+  productsResource = rxResource({
+    request: () => ({ gender: this.gender() }),
+    loader: ({ request }) => {
+      return this.productsService.getProducts({
+        gender: request.gender,
+      });
+    },
+  });
 }
