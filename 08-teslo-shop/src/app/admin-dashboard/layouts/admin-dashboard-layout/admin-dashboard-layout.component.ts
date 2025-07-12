075f427a07626542cfa85d3b852840f682db0ae7
caindra <mmirgal868@g.educaand.es>
2025-07-12 20:36:38 +0200

    admin dashboard design

--- 08-teslo-shop/src/app/admin-dashboard/layouts/admin-dashboard-layout/admin-dashboard-layout.component.ts
+++ 08-teslo-shop/src/app/admin-dashboard/layouts/admin-dashboard-layout/admin-dashboard-layout.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AuthService } from '@auth/services/auth.service';
 
 @Component({
   selector: 'app-admin-dashboard-layout',
-  imports: [
-    RouterOutlet,
-  ],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './admin-dashboard-layout.component.html',
-  styles: ``
+  styles: ``,
 })
 export class AdminDashboardLayoutComponent {
+  authService = inject(AuthService);
 
+  user = computed(() => this.authService.user());
 }
